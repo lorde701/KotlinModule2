db4f9ecf1ea01e9d1a1889691749ef516210fe35
lorde701 <ivanka0701@mail.ru>
2019-08-18 10:58:30 +0400

    move to module2

--- src/module2/Task8.kt
+++ src/module2/Task8.kt
@@ -1,33 +1,33 @@
 //Задание 8: Проценты по вкладу
-//Напишите программу для расчета дохода по вкладу. Пользователь вводит сумму вклада, длительность вклада (количество месяцев) и ежемесячный процент по вкладу. Программа выводит в консоль информацию на сколько будет увеличиваться вклад каждый месяц.
+//Напишите программу для расчета дохода по вкладу. Пользователь вводит сумму вклада, длительность вклада (количество месяцев) и ежемесячный процент по вкладу.
+// Программа выводит в консоль информацию на сколько будет увеличиваться вклад каждый месяц.
 
 fun main(array: Array<String>) {
     println("Введите сумму вклада")
-    val depositAmount: Double
-    try {
-        depositAmount = readLine().toString().replace(",", ".").toDouble()
-    } catch (e: NumberFormatException) {
-        println("Сумма вклада должна быть числом")
-        return
-    }
+    val depositAmount =
+            try {
+                readLine().toString().replace(",", ".").toDouble()
+            } catch (e: NumberFormatException) {
+                println("Сумма вклада должна быть числом")
+                return
+            }
 
     println("Введите длительность вклада в месяцах")
-    val depositDuration: Int
-    try {
-        depositDuration = readLine().toString().toInt()
-    } catch (e: NumberFormatException) {
-        println("Длительность вклада должна быть числом")
-        return
-    }
+    val depositDuration =
+            try {
+                readLine().toString().toInt()
+            } catch (e: NumberFormatException) {
+                println("Длительность вклада должна быть числом")
+                return
+            }
 
     println("Введите процент по вкладу")
-    val percent: Double
-    try {
-        percent = readLine().toString().replace(",", ".").toDouble()
-    } catch (e: NumberFormatException) {
-        println("Процент по вкладу должен быть числом")
-        return
-    }
+    val percent =
+            try {
+                readLine().toString().replace(",", ".").toDouble()
+            } catch (e: NumberFormatException) {
+                println("Процент по вкладу должен быть числом")
+                return
+            }
     println("Сумма, которую Вы получите, равна ${depositAmount * percent / 100 * depositDuration}")
-
 }
